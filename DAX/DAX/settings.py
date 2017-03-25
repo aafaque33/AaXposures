e3a25cc6bdf0db954300b4f611886e19a3c46d25
aafaque33 <aafaqueabdullah@yahoo.com>
2017-03-24 21:36:01 -0500

    edit settings

--- DAX/DAX/settings.py
+++ DAX/DAX/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&y(=e(#fm$8(3d=p4d(en87p28is=i--%!j9-xejci(1gg)%3f'
+SECRET_KEY = 'yourkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
